d6ef327e48227527d0db7cec442d538576f8ebe8
Johannes <johannes.wikner@gmail.com>
2021-02-22 20:35:10 +0100

    manifest_v2

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener(function (tabId,changeInfo, {url}) {
-  if (url.startsWith("chrome://")) return;
-  chrome.scripting.executeScript({
-    target: {tabId, allFrames: true},
-    files: ["content.js"]
-  });
-})
