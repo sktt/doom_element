9376526c57ce833c502757ad53b3872734de40ac
Johannes <johannes.wikner@gmail.com>
2021-02-22 13:49:04 +0100

    stuff

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-chrome.tabs.onUpdated.addListener(function (tabId,changeInfo) {
+chrome.tabs.onUpdated.addListener(function (tabId,changeInfo, {url}) {
+  if (url.startsWith("chrome://")) return;
   chrome.scripting.executeScript({
     target: {tabId, allFrames: true},
     files: ["content.js"]
   });
 })
-
